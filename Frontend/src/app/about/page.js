340881af94e3c324a949d2f7763151efd9471243
Saif CNIT <jubayerhossain111220@gmail.com>
2025-02-05 16:59:30 +0600

    added pages

---
+++ Frontend/src/app/about/page.js
@@ -0,0 +1,13 @@
+import About from "@/Pages/WebsitePages/About/About";
+import React from "react";
+import "@/assets/css/about.css"
+
+const AboutPage = () => {
+  return (
+    <>
+      <About />
+    </>
+  );
+};
+
+export default AboutPage;

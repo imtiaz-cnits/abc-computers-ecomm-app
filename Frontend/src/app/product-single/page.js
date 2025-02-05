d9aa1ec8c5a6d16447c61be86509da366cd4a9be
JubayerCodes <jubayerhossain111220@gmail.com>
2025-02-05 10:29:56 +0600

    completed single product page

--- Frontend/src/app/product-single/page.js
+++ Frontend/src/app/product-single/page.js
@@ -1,6 +1,10 @@
 import SingleProduct from '@/Pages/WebsitePages/SingleProduct/SingeProduct';
 import React from 'react';
 
+export const metadata = {
+    title: "Product Single | ABC Computers"
+}
+
 const SingleProductPage = () => {
     return (
         <>

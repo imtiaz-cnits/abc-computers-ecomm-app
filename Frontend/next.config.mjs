ebddb957c63eed9b541beab569b16563eda386ca
Saif CNIT <jubayerhossain111220@gmail.com>
2025-02-09 18:06:33 +0600

    added styles in add product forms

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    productionBrowserSourceMaps: false
+  productionBrowserSourceMaps: false,
+
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;

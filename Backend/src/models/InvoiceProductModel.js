dba44b33f83fdd4f893c5a13b9f19e6880d059d8
imtiaz-cnits <ia64744@gmail.com>
2025-03-18 14:51:32 +0600

    simple push

--- Backend/src/models/InvoiceProductModel.js
+++ Backend/src/models/InvoiceProductModel.js
@@ -3,9 +3,8 @@ const DataSchema = mongoose.Schema(
   {
     productID: { type: mongoose.Schema.Types.ObjectId, required: true },
     billingDetailID: { type: mongoose.Schema.Types.ObjectId, required: true },
+    orderID: {type: String, required: true },
     qty: { type: String, required: true },
-    price: { type: String, required: true },
-    size: { type: String, required: true },
     color: { type: String, required: true },
   },
   { timestamps: true, versionKey: false }
--- Backend/src/models/PaymentModel.js
+++ Backend/src/models/PaymentModel.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 const DataSchema = mongoose.Schema(
   {
     invoiceID: { type: mongoose.Schema.Types.ObjectId, required: true },
-    pay_method: { type: String },
+    subTotal: { type: String, required: true },
+    discount: { type: String, required: true },
+    grandTotal: { type: String, required: true },
+    pay_method: { type: String, required: true },
     tran_id: { type: String },
     acc_number: { type: String },
+    payment_status: { type: String, required: true },
   },
   { timestamps: true, versionKey: false }
 );

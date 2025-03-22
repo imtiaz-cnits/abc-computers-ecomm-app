b367851cc46e3968f4ccae5a1880e9e566350622
Saif CNIT <jubayerhossain111220@gmail.com>
2025-03-22 16:38:08 +0600

    added status update

--- Backend/app.js
+++ Backend/app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:3000", // Replace with your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"], // Allow Authorization header
     credentials: true, // Allow cookies (if needed)
   })
